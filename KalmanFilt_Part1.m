a9e6eb1fbf07455a91753ac8303d380c085f8844
freshdk8838 <47158383+freshdk8838@users.noreply.github.com>
2020-03-14 01:41:19 -0400

    Add files via upload

---
+++ KalmanFilt_Part1.m
@@ -0,0 +1,15 @@
+clear; % Clear variables
+datasetNum = 4; % CHANGE THIS VARIABLE TO CHANGE DATASET_NUM
+[sampledData, sampledVicon, sampledTime] = init(datasetNum);
+%% How do you want to do this version
+Z = sampledVicon(1:6,:);
+% Set initial condition
+uPrev = vertcat(sampledVicon(1:9,1),zeros(6,1)); % Copy the Vicon Initial state
+covarPrev = eye(15); % Covariance constant
+savedStates = zeros(15, length(sampledTime)); %J ust for saving state his.
+prevTime = 0; %last time step in real time
+for i = 1:length(sampledTime)
+    %% Fill in the FOR LOOP
+end
+
+plotData(savedStates, sampledTime, sampledVicon, 1, datasetNum);
